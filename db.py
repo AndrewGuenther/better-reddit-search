e33eefad7896c9566f6248c6988b71a17338cec1
Andrew Guenther <guenther.andrew.j@gmail.com>
2012-05-31 12:40:18 -0700

    Issue with Heroku Postgres, testing possible solution

--- db.py
+++ db.py
@@ -50,7 +50,7 @@ cur.execute("CREATE OR REPLACE FUNCTION sim(text, text[], integer) RETURNS float
    LANGUAGE SQL\
    IMMUTABLE;\
 ")
-cur.execute("CREATE OR REPLACE FUNCTION search(text[], integer) RETURNS table(id char(5), title varchar(300), link text, rank float)\
+cur.execute("CREATE OR REPLACE FUNCTION search(text[], integer) RETURNS table (id char(5), title varchar(300), link text, rank float)\
    AS 'select id, title, link, wilson(ups, downs) * ((1.5 * sim(id, $1, 0)) + (1.25 * sim(id, $1, 1)) + sim(id, $1, 2)) as rank from post order by rank desc limit $2;'\
    LANGUAGE SQL\
    IMMUTABLE;")\
